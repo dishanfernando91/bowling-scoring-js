8358c28739f26540d85de3d43d80d9cc5b13b267
Dishan Fernando <dmfernando91@gmail.com>
2025-07-26 16:56:42 +0530

    cleaned up code, fixed undefined bug

--- src/index.js
+++ src/index.js
@@ -16,29 +16,27 @@ const SAMPLE_GAME = [
 
 const calculateFrameScore = (currentFrameIndex, allFrames) => {
     let currentFrameScore = 0;
+    const currentFrame = allFrames[currentFrameIndex]
 
-    currentFrameScore = allFrames[currentFrameIndex].reduce((attempt, total) => attempt + total, 0)
+    currentFrameScore = currentFrame.reduce((attempt, total) => attempt + total, 0)
 
         if (currentFrameIndex !== allFrames.length - 1) {
-            if (allFrames[currentFrameIndex].length === 1) {
+            if (currentFrame.length === 1) {
             return currentFrameScore += calculateFrameScore(currentFrameIndex + 1, allFrames)
         } 
 
-        if (currentFrameScore === MAX_FRAME_SCORE && allFrames[currentFrameIndex].length > 1) {
-            // get the points from the first round of thenext frame and add to the current frame's (which is 10) for spares
+        if (currentFrameScore === MAX_FRAME_SCORE && currentFrame.length > 1) {
+            // get the points from the first round of the next frame and add to the current frame's (which is 10) for spares
             return currentFrameScore += allFrames[currentFrameIndex + 1][0] 
         }
     }
     return currentFrameScore
 }
 
-const calculateTotalScore = allFrames => {
-    return allFrames.reduce((totalScore, _, currentFrameIndex, allFrames) => {
-        return (calculateFrameScore(currentFrameIndex, allFrames) + totalScore)
-    }, INITIAL_SCORE);
-}
+const calculateTotalScore = allFrames => allFrames.reduce((totalScore, _, currentFrameIndex, allFrames) =>
+        calculateFrameScore(currentFrameIndex, allFrames) + totalScore, INITIAL_SCORE);
 
 // add custom game data through CLI or use sample
-const finalScore = calculateTotalScore(JSON.parse(process.argv[2]) ?? SAMPLE_GAME)
+const finalScore = calculateTotalScore(process.argv[2] ? JSON.parse(process.argv[2]) : SAMPLE_GAME)
 
 console.log(finalScore)
