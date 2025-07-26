8cb289708c7d83a1c7745f56866304761fde03f2
Dishan Fernando <dmfernando91@gmail.com>
2025-07-26 17:02:36 +0530

    more code formatting

--- src/index.js
+++ src/index.js
@@ -16,28 +16,28 @@ const SAMPLE_GAME = [
 
 const calculateFrameScore = (currentFrameIndex, allFrames) => {
     let currentFrameScore = 0;
-    const currentFrame = allFrames[currentFrameIndex]
+    const currentFrame = allFrames[currentFrameIndex];
 
-    currentFrameScore = currentFrame.reduce((attempt, total) => attempt + total, 0)
+    currentFrameScore = currentFrame.reduce((attempt, total) => attempt + total, 0);
 
     if (currentFrameIndex !== allFrames.length - 1) {
         if (currentFrame.length === 1) {
             return currentFrameScore += calculateFrameScore(currentFrameIndex + 1, allFrames)
-        } 
+        };
 
         if (currentFrameScore === MAX_FRAME_SCORE && currentFrame.length > 1) {
             // get the points from the first round of the next frame and add to the current frame's (which is 10) for spares
             return currentFrameScore += allFrames[currentFrameIndex + 1][0] 
-        }
+        };
     }
     
-    return currentFrameScore
+    return currentFrameScore;
 }
 
 const calculateTotalScore = allFrames => allFrames.reduce((totalScore, _, currentFrameIndex, allFrames) =>
         calculateFrameScore(currentFrameIndex, allFrames) + totalScore, INITIAL_SCORE);
 
 // add custom game data through CLI or use sample
-const finalScore = calculateTotalScore(process.argv[2] ? JSON.parse(process.argv[2]) : SAMPLE_GAME)
+const finalScore = calculateTotalScore(process.argv[2] ? JSON.parse(process.argv[2]) : SAMPLE_GAME);
 
-console.log(finalScore)
+console.log(finalScore);
